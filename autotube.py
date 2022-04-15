1a6e10efa8737e287cdb04d6bdf2d5d0d2f65c77
Giovanni <giovanni.andrade9991@gmail.com>
2022-04-14 22:47:15 -0300

    Initial autotube channel creation

---
+++ autotube.py
@@ -0,0 +1,31 @@
+"""
+This is the main loop file for our AutoTube Bot!
+Quick notes!
+- Currently it's set to try and post a video then sleep for a day.
+- You can change the size of the video currently it's set to post shorts.
+    * Do this by adding a parameter of scale to the image_save function.
+    * scale=(width,height)
+"""
+
+from datetime import date
+import time
+from utils.CreateMovie import CreateMovie, GetDaySuffix
+from utils.RedditBot import RedditBot
+from utils.upload_video import upload_video
+
+#Create Reddit Data Bot
+redditbot = RedditBot()
+
+# Leave if you want to run it 24/7
+while True:
+
+    # Gets our new posts pass if image related subs. Default is memes
+    posts = redditbot.get_posts("memes")
+
+    # Create folder if it doesn't exist
+    redditbot.create_data_folder()
+
+    # Go through posts and find 5 that will work for us.
+    for post in posts:
+        redditbot.save_image(post)
+
