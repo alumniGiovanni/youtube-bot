87418b02e202a81d63c093d55d7104e650cd6c7a
Giovanni <giovanni.andrade9991@gmail.com>
2022-04-14 22:48:45 -0300

    added video creation and description

--- viewers.py
+++ viewers.py
@@ -13,3 +13,25 @@ for i in range(5):
     DAY = date.today().strftime("%d")
     DAY = str(int(DAY)) + GetDaySuffix(int(DAY))
     dt_string = date.today().strftime("%A %B") + f" {DAY}"
+
+
+    # Create the movie itself!
+    CreateMovie.CreateMP4(redditbot.post_data)
+
+    # Video info for YouTube.
+    # This example uses the first post title.
+    video_data = {
+            "file": "video.mp4",
+            "title": f"{redditbot.post_data[0]['title']} - Dankest memes and comments {dt_string}!",
+            "description": "#shorts\nGiving you the hottest memes of the day with funny comments!",
+            "keywords":"meme,reddit,Dankestmemes",
+            "privacyStatus":"public"
+    }
+
+    print(video_data["title"])
+    print("Posting Video in 5 minutes...")
+    time.sleep(60 * 5)
+    upload_video(video_data)
+
+    # Sleep until ready to post another video!
+
