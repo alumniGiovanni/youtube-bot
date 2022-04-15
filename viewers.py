326a27ca68845fa679a54a16b162e4b77314ec46
Giovanni <giovanni.andrade9991@gmail.com>
2022-04-14 22:47:56 -0300

    adding datetime to videos

--- viewers.py
+++ viewers.py
@@ -8,3 +8,8 @@ Duration = input(5)
 for i in range(5):
   webrowser.open_new(url)
   time.sleep(int(Duration))
+
+    # Wanted a date in my titles so added this helper
+    DAY = date.today().strftime("%d")
+    DAY = str(int(DAY)) + GetDaySuffix(int(DAY))
+    dt_string = date.today().strftime("%A %B") + f" {DAY}"
