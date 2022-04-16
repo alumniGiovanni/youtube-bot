14f12f2cbf1dab1aae9f0d46d64476a5fc1bb434
Giovanni <giovanni.andrade9991@gmail.com>
2022-04-15 22:27:49 -0300

    creating videos

--- autotube.py
+++ autotube.py
@@ -29,3 +29,29 @@ while True:
     for post in posts:
         redditbot.save_image(post)
 
+
+    # Wanted a date in my titles so added this helper
+    DAY = date.today().strftime("%d")
+    DAY = str(int(DAY)) + GetDaySuffix(int(DAY))
+    dt_string = date.today().strftime("%A %B") + f" {DAY}"
+
+    # Create the movie itself!
+    CreateMovie.CreateMP4(redditbot.post_data)
+
+    # Video info for YouTube.
+    # This example uses the first post title.
+    video_data = {
+            "file": "video.mp4",
+            "title": f"{redditbot.post_data[0]['title']} - Dankest memes and comments {dt_string}!",
+            "description": "#shorts\nGiving you the hottest memes of the day with funny comments!",
+            "keywords":"meme,reddit,Dankestmemes",
+            "privacyStatus":"public"
+    }
+
+    print(video_data["title"])
+    print("Posting Video in 5 minutes...")
+    time.sleep(60 * 5)
+    upload_video(video_data)
+
+    # Sleep until ready to post another video!
+    time.sleep(60 * 60 * 24 - 1)
